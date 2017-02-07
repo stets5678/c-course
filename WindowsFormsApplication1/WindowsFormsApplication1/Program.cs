043333d5866b74f7e46eafb7ab98cb5118755ca3
oleh <spycer.lviv@gmail.com>
2017-02-07 20:30:46 +0200

    Added first lesson

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_1
+{
+     class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Logos");
+        }
+    }
+}
